30924f82771cf1e1e27ede0cb71fb7b4864ac599
jdw1227 <jdw1227@midasit.com>
2023-11-02 14:41:49 +0900

    회원가입 시 ucehf 인증 키 검증

--- src/main/kotlin/com/midasit/mcafe/domain/member/dto/MemberRequest.kt
+++ src/main/kotlin/com/midasit/mcafe/domain/member/dto/MemberRequest.kt
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 class MemberRequest {
     @Schema(description = "회원가입", name = "MemberRequestSignup")
-    class Signup(val phone: String, val name: String, val password: String)
+    class Signup(val phone: String, val name: String, val password: String, val certKey: String)
 
     @Schema(description = "로그인", name = "MemberRequestLogin")
     class Login(val phone: String, val password: String)
